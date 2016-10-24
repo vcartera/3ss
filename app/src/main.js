0b6903ae76a7caa29ca96ee6e17fbd819d0115ac
Vitalie Cartera <vitalie_cartera@yahoo.com>
2016-10-24 20:14:51 +0300

    add dynamic images. cleanup

--- app/src/main.js
+++ app/src/main.js
@@ -13,54 +13,45 @@ var item = function (image, title, content) {
 var scrollBoxContainer = document.getElementById("scroll-box");
 var headlineContainer = document.getElementById("headline");
 
-scrollBoxContainer.innerHTML = "";
-var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin hendrerit metus eu metus finibus, ut bibendum ante gravida. Nulla facilisi. Pellentesque mi mi, elementum sit amet lorem in, luctus porttitor mauris. Nam convallis, dolor sed efficitur aliquet, lectus augue scelerisque magna, in egestas felis nibh et arcu."
-
-for (var i = 0; i < 10; i++) {
-    scrollBoxContainer.appendChild(item(
-        'https://i.ytimg.com/vi/ll6XE5J9SUY/hqdefault.jpg?custom=true&w=168&h=94&stc=true&jpg444=true&jpgq=90&sp=68&sigh=Cx1nnRSpoljKN5ix-LM0A0kd5I4',
-        "Title is long and ugly needs to shave more often",
-        lorem));//'<div id="item">' + lorem + '</div>';
-}
-
 var request = new XMLHttpRequest();
 
 request.onreadystatechange = readyCallback;
 
 function readyCallback() {
-    if( request.readyState < 4 || request.status !== 200){
+    if( request.readyState < 4 ){
+        return;
+    }
+    if(request.status !== 200){
         scrollBoxContainer.innerHTML = "Error fetching data";
         return;
     }
 
     if(request.responseText != ''){
 
-        var xml = request.responseXML.getElementsByTagName("channel")[0].getElementsByTagName("item");
-        var xml2 = request.responseXML.getElementsByTagName("channel")[0];
+        var ns = request.responseXML.getElementsByTagName("rss")[0].attributes["xmlns:media"].value;
+
+        var items = request.responseXML.getElementsByTagName("channel")[0].getElementsByTagName("item");
+        var xml = request.responseXML.getElementsByTagName("channel")[0];
 
         // update headline
-        var headline = xml2.getElementsByTagName("title")[0].innerHTML;
+        var headline = xml.getElementsByTagName("title")[0].innerHTML;
         if(headline != undefined){
             headlineContainer.innerHTML = stripCDATA(headline);
         }
 
-        var items = xml2.childNodes;
-        console.log(items);
-
-        var ns = "http://search.yahoo.com/mrss/";
-        console.log(ns);
-
         // clean up scroll box container
         scrollBoxContainer.innerHTML = "";
-        for (var i = 0; i < /*xml.length*/ 1; i++){
-            var title = xml[i].getElementsByTagName("title")[0].innerHTML
+
+        // generate content
+        for (var i = 0; i < items.length; i++){
+            var title = items[i].getElementsByTagName("title")[0].innerHTML;
             title = stripCDATA(title);
-            var description = xml[i].getElementsByTagName("description")[0].innerHTML
-            var link = xml[i].getElementsByTagName("link")[0].innerHTML;
-            var group = xml[i].getElementsByTagNameNS(ns, "group")[0];
-            var image = group.firstChild.xml;
-            console.log('imag: ' + image);
-            scrollBoxContainer.appendChild(item( null, title, description));
+            var description = items[i].getElementsByTagName("description")[0].innerHTML;
+            description = stripImageTags(description);
+            var link = items[i].getElementsByTagName("link")[0].innerHTML;
+            var group = items[i].getElementsByTagNameNS(ns, "group")[0];
+            var image = group.firstChild.attributes["url"].value;
+            scrollBoxContainer.appendChild(item( image, title, description));
         }
 
     }
@@ -68,6 +59,10 @@ function readyCallback() {
 
 function stripCDATA(data){
     return data.replace(/<!\[CDATA\[(.*?)\]\]>/g, '$1' );
+}
+
+function stripImageTags(data){
+    return data.replace(/&lt;\/?[^>]+(&gt;|$)/g, '[link]');
 }
 
 request.open("GET", "https://crossorigin.me/http://rss.cnn.com/rss/edition_world.rss" , true);
